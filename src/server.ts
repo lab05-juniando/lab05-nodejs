c3314f2170d5f0d92837dc085f2233230707271c
Lucas Lima <lucaslima@MacBook-Pro-de-Lucas.local>
2026-08-05 10:07:42 -0300

    test: action

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World! NodeJS");
+  res.send("Hello World! NodeJS.");
 });
 
 server.listen(process.env.PORT, () => {
