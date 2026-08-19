75f48de95108bc056cb2f6052fa232a148889a97
joaopedro029 <joaopedrosantoss029@gmail.com>
2026-08-18 23:06:02 -0300

    fix/endpoint-profile

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { router as RouteRegister } from "../modules/user/routes/register.route";
 import { routerAuth } from "../modules/auth/Routers/auth.router";
+import { routerUser } from "../modules/auth/Routers/User.router";
 
 const router = Router();
 
 router.use("/users", RouteRegister);
 router.use("/login", routerAuth);
+router.use("/users", routerUser);
 
 export default router;
